88bf48be4af93da35106df5f99467d883ef536a2
Darsh Lakhwani <darshlakhwani@Darshs-MacBook-Air.local>
2020-09-30 23:24:21 +0530

    added log in

---
+++ login.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>S.P.I.N</title>
+  <link rel="stylesheet" href="css/styles.css">
+  <link href="https://fonts.googleapis.com/css2?family=Merriweather&family=Montserrat&family=Open+Sans&family=Pacifico&family=Poppins&family=Sacramento&display=swap" rel="stylesheet">
+
+<body>
+
+  <!-- top contains the headings -->
+
+  <div class="top">
+
+    <h1 class='title'>S.P.I.N</h1>
+    <h3>Sardar Patel Institutional Network</h3>
+
+  </div>
+
+  <div>
+
+
+
+    <form class="form" action="index.html" method="post">
+
+      <!-- Unordered list without bullets -->
+
+      <ul style="list-style-type:none;">
+
+        <br />
+
+        <li>
+
+          <!-- placeholer is for greyish text -->
+
+          <input class = 'input' type="text" name="" placeholder="Username or Email Address">
+
+        </li>
+
+        <br />
+
+        <li>
+
+          <input class = 'input' type="password" name="" placeholder="Password">
+
+        </li>
+
+        <br />
+
+        <li>
+
+          <input class = 'login' type="submit" name="" value="Log In">
+
+        </li>
+
+        <hr size='3' />
+
+
+      </ul>
+
+      <h3 class = 'newhere'>New here?</h3>
+
+      <a class = 'create' href='load.php'>Create a New Account</a>
+
+
+
+
+    </form>
+
+
+
+
+  </div>
+
+
+
+</body>
+
+</html>

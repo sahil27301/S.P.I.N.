3d516a8413eb9a0e20fb6b20359c6164609550a9
sahil27301 <65967824+sahil27301@users.noreply.github.com>
2020-10-10 22:15:49 +0530

    Revert "Added partials"

--- partials/navbar.php
+++
@@ -1,9 +0,0 @@
-<nav class="navbar sticky-top" style="width:100%;z-index:1;">
-    <button class="openbtn" onclick="toggleNav()">
-        <i class="fa fa-bars fa-2x"></i>
-    </button>
-    <h2>S.P.I.N</h2>
-    <a href="/spin/login/login.php">
-    <button id="logout">Logout</button>
-    </a>
-</nav>
